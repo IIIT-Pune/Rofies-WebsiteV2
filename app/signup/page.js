3ad7f331026e6fe599ecb54e862a3e9d775c6bdc
KrishKoria <krishkoria2004@gmail.com>
2024-06-22 00:18:34 +0530

    Login page ui changes and login functionality added

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,73 @@
-import { SignupForm } from "@/components/signupform";
-
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Globe } from "@/components/globe";
+import { Signup } from "@/lib/actions";
+import { IconBrandGoogle } from "@tabler/icons-react";
 export default function SignupPage() {
-  return <SignupForm />;
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Signup</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your information below to get started
+            </p>
+          </div>
+
+          <div className="grid gap-4">
+            <form action={Signup}>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  name="username"
+                  placeholder="John Doe"
+                  required
+                  className="p-4"
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email" className="pt-2">
+                  Email
+                </Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2 pb-4">
+                <Label htmlFor="password" className="pt-2">
+                  Password
+                </Label>
+                <Input id="password" type="password" required name="password" />
+              </div>
+              <Button className="w-full">Signup</Button>
+            </form>
+            <hr className="my-4 border-t border-black dark:border-white" />
+            <Button variant="outline" className="w-full">
+              <IconBrandGoogle size={20} />
+              <span className="ml-2">Signup with Google</span>
+            </Button>
+          </div>
+
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/login" className="underline">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Globe />
+      </div>
+    </div>
+  );
 }
