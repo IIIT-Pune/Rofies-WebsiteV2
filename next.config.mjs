27cf63b22f8d70c4ae3c6a13bb755557c1316f47
KrishKoria <krishkoria2004@gmail.com>
2024-07-01 21:38:44 +0530

    bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    instrumentationHook: true,
-  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
