da7c55551cee7067b2381928f64e3038d551c059
KrishKoria <krishkoria2004@gmail.com>
2024-07-01 13:24:24 +0530

    refactored the logout flow #2

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,23 +1,25 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { LogoutAction } from "@/lib/actions";
-
 export default function ProfilePage() {
   const handleLogoutSubmitHandler = async (e) => {
-    e.preventDefault();
-    const res = await LogoutAction();
-    if (res.error) {
-      console.error(res.error);
-    }
-    if (res.success) {
-      console.log("Logout successful");
-      window.location.href = "/";
+    try {
+      e.preventDefault();
+      const response = await fetch("/api/logout");
+      const result = await response.json();
+      if (result.success) {
+        console.log("Logout successful");
+        window.location.href = "/";
+      } else {
+        console.error(result.error || "Logout failed");
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
   return (
     <>
+      <h1 className="text-orange-400">Profile Page</h1>
       <form onSubmit={handleLogoutSubmitHandler}>
-        <h1>Profile Page</h1>
         <Button type="submit">Logout</Button>
       </form>
     </>
