552eb52ad7139f963087748610987587fd838d1b
KrishKoria <krishkoria2004@gmail.com>
2024-07-03 17:52:59 +0530

    minor fixes

--- components/3dcard.js
+++ components/3dcard.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/3dcard";
 
 export function ThreeDCard({ id, public_id, format, blurDataUrl }) {
