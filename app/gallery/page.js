c4e31f2c8b69bd2fe746ced0a6db2c7a0f44faac
KrishKoria <krishkoria2004@gmail.com>
2024-07-03 17:50:42 +0530

    fixed loading maybe

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -23,15 +23,13 @@ export default function Home() {
       {isLoading && <GalleryLoading />}
       <div className="grid gap-x-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {images.map(({ id, public_id, format, blurDataUrl }) => (
-          <Suspense key={id} fallback={<Loading />}>
-            <ThreeDCard
-              key={id}
-              id={id}
-              public_id={public_id}
-              format={format}
-              blurDataUrl={blurDataUrl}
-            />
-          </Suspense>
+          <ThreeDCard
+            key={id}
+            id={id}
+            public_id={public_id}
+            format={format}
+            blurDataUrl={blurDataUrl}
+          />
         ))}
       </div>
     </>
